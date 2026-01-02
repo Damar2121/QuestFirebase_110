9ce03b9f6e33d3210751cf3124d61c0af183803a
Damar2121 <damar.vivoy51@gmail.com>
2026-01-02 16:04:33 +0700

    mengcoding isi untuk EntryViewModel.kt

--- app/src/main/java/com/example/pertemuan14/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/viewmodel/EntryViewModel.kt
@@ -1,4 +1,30 @@
 package com.example.pertemuan14.viewmodel
 
-class EntryViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan14.modeldata.DetailSiswa
+import com.example.pertemuan14.repositori.RepositorySiswa
+
+class EntryViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean
+    {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            repositorySiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+        }
+    }
 }
