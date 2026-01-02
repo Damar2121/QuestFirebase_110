13a517717c894c8caa9bf14d3012f184b009f608
Damar2121 <damar.vivoy51@gmail.com>
2026-01-02 16:19:11 +0700

    mengcoding isi untuk HalamanHome.kt

--- app/src/main/java/com/example/pertemuan14/view/HalamanHome.kt
+++ app/src/main/java/com/example/pertemuan14/view/HalamanHome.kt
@@ -1,2 +1,206 @@
 package com.example.pertemuan14.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan14.R
+import com.example.pertemuan14.modeldata.Siswa
+import com.example.pertemuan14.view.route.DestinasiHome
+import com.example.pertemuan14.viewmodel.HomeViewModel
+import com.example.pertemuan14.viewmodel.PenyediaViewModel
+import com.example.pertemuan14.viewmodel.StatusUiSiswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    navigateToItemUpdate: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ){
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.entry_siswa)
+                )
+            }
+        },
+    ) { innerPadding ->
+        HomeStatus(
+            statusUiSiswa = viewModel.statusUiSiswa,
+            retryAction = { viewModel.loadSiswa() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = navigateToItemUpdate,
+            onDeleteClick = {
+                // viewModel.deleteSiswa(it) // Implement delete logic
+                viewModel.loadSiswa()
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeStatus(
+    statusUiSiswa: StatusUiSiswa,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Siswa) -> Unit = {},
+    onDetailClick: (String) -> Unit
+){
+    when (statusUiSiswa) {
+        is StatusUiSiswa.Loading -> OnLoading(modifier = modifier)
+        is StatusUiSiswa.Success -> SiswaLayout(
+            siswa = statusUiSiswa.siswa,
+            modifier = modifier.fillMaxWidth(),
+            onDetailClick = {
+                onDetailClick(it.id.toString())
+            },
+            onDeleteClick = {
+                onDeleteClick(it)
+            }
+        )
+        is StatusUiSiswa.Error -> OnError(retryAction, modifier = modifier)
+    }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.loading_img), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.gagal), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun SiswaLayout(
+    siswa: List<Siswa>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Siswa) -> Unit,
+    onDeleteClick: (Siswa) -> Unit = {}
+){
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(siswa) { orang ->
+            SiswaCard(
+                siswa = orang,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(orang) },
+                onDeleteClick = {
+                    onDeleteClick(orang)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun SiswaCard(
+    siswa: Siswa,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Siswa) -> Unit = {}
+){
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = siswa.nama,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { onDeleteClick(siswa) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                    )
+                }
+            }
+            Text(
+                text = siswa.telpon,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = siswa.alamat,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
