d915c9b18efcabb74a8afc386a76bcf829a8f6ed
Damar2121 <damar.vivoy51@gmail.com>
2026-01-02 16:02:42 +0700

    mengcoding isi untuk HomeViewModel.kt

--- app/src/main/java/com/example/pertemuan14/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/viewmodel/HomeViewModel.kt
@@ -1,2 +1,35 @@
 package com.example.pertemuan14.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan14.modeldata.Siswa
+import com.example.pertemuan14.repositori.RepositorySiswa
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() {
+    var statusUiSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init{
+        loadSiswa()
+    }
+
+    fun loadSiswa(){
+        viewModelScope.launch {
+            statusUiSiswa = StatusUiSiswa.Loading
+            statusUiSiswa = try {
+                StatusUiSiswa.Success(repositorySiswa.getDataSiswa())
+            }catch (e: IOException){
+                StatusUiSiswa.Error
+            }
+            catch (e:Exception){
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
